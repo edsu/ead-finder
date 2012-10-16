3aabe7fd5268a531165e18ada4d847521c384387
Ed Summers <ehs@pobox.com>
2012-10-16 14:51:17 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+ead-finder
+==========
+
+ead-finder is a small Node utility that looks for EAD XML that has been 
+indexed by Google.
+
+Install
+-------
+
+1. sudo apt-get install nodejs redis-server
+1. git checkout https://github.com/edsu/ead-finder.git
+1. cd ead-finder
+1. npm install
+1. node crawl.js
+1. redis-dump --json > dump.json
+
+License:
+--------
+
+* CC0
---
+++ crawl.js
@@ -0,0 +1,99 @@
+/**
+ * uses google to try to find EAD XML documents out on the 
+ * World Wild Web. Uses redis to keep track of results and 
+ * serializes them to dump.json when finished.
+ */
+
+var fs = require("fs"),
+    url = require("url"),
+    async = require("async"),
+    http = require("http"),
+    google = require("google"),
+    request = require("request"),
+    libxmljs = require("libxmljs"),
+    redisdump = require("redis-dump"),
+    redis = require("redis").createClient();
+
+http.globalAgent.maxSockets = 2;
+
+function main() {
+  async.series([crawl, dump, exit]);
+}
+
+/**
+ * Crawl google and return a dictionary of hostnames and sample
+ * ead files.
+ */
+
+function crawl(opts) {
+  if (! opts) opts = {};
+  var sleepMillis = opts.sleep || 5000;
+  var maxPages = opts.maxPages || -1;
+  var nextCounter = 0;
+  google.resultsPerPage = 25;
+  
+  google("ead filetype:xml", function(err, next, links) {
+    if (err) {
+      console.log("unable to fetch google search results: " + err); 
+      return;
+    }
+  
+    // add hits to redis if they are EAD XML docs
+    for (var i = 0; i < links.length; ++i) {
+      isEAD(links[i].link, function(u) {
+        redis.sadd(url.parse(u).host, u);
+      });
+    }
+
+    // process next page
+    if (nextCounter < maxPages || maxPages == -1) {
+      sleep(sleepMillis);
+      nextCounter += 1;
+      if (next) { 
+        console.log("\nfetching page " + nextCounter);
+        next();
+      } else {
+        redis.quit();
+      }
+    }
+
+  });
+
+}
+
+function dump() {
+  redisdump({format: "json"}, function(err, result) {
+    console.log(err);
+    fs.writeFile("dump.json", result);
+  });
+}
+
+function exit() {
+  redis.quit();
+}
+
+function isEAD(url, callback) {
+  request(url, function(err, response, body) {
+    if (err) {
+      console.log("can't fetch " + url);
+      return;
+    }
+
+    try {
+      var doc = libxmljs.parseXml(body);
+      if (doc.root().name() == "ead") {
+        console.log("found ead xml: " + url);
+        callback(url);
+      }
+    } catch(e) {
+      console.log(url + " is not xml");
+    }
+  });
+}
+
+function sleep(milliSeconds) {
+  var startTime = new Date().getTime();
+  while (new Date().getTime() < startTime + milliSeconds);
+}
+
+main();
