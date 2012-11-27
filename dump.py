bbbf936135f48a43ae9077e61febd86646b17af3
Ed Summers <ehs@pobox.com>
2012-11-27 16:06:05 -0500

    modified dump json format to aggregate under host

--- dump.py
+++ dump.py
@@ -3,11 +3,13 @@
 import json
 import redis
 
-stats = {"html": {}, "xml": {}}
+stats = {}
 db = redis.Redis()
 
 for key in db.keys("*"):
     host, content_type = key.split("__")
-    stats[content_type][host] = list(db.smembers(key))
+    if not stats.get_key(host):
+        stats[host] = {}
+    stats[host][content_type] = list(db.smembers(key))
 
 open("dump.json", "w").write(json.dumps(stats, indent=2))
